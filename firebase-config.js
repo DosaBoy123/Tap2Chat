da11282f092deca976032f3a4b058a2b0f5cf1c7
DosaBoy123 <dosaboy67@gmail.com>
2026-05-28 20:38:54 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,18 +2,18 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
+// Your real Firebase config
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_SENDER",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyC4RxS1VpdyP3oMPtiJH2nqlA7Y7OM5t-A",
+  authDomain: "tap2chat-13249.firebaseapp.com",
+  projectId: "tap2chat-13249",
+  storageBucket: "tap2chat-13249.firebasestorage.app",
+  messagingSenderId: "109302813848",
+  appId: "1:109302813848:web:7cb0386f29598ec27e2ced"
 };
 
+// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storage = getStorage(app);
